1246d2853dcea33b5c0d214e7c2cb5d15f3e399d
lian <duanliandba@gmail.com>
2021-03-27 07:25:26 -0400

    add version end point

--- src/main/java/com/docker/restful/demo/EndpointVersion.java
+++ src/main/java/com/docker/restful/demo/EndpointVersion.java
@@ -3,17 +3,20 @@
 package com.docker.restful.demo;
 
 
-import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-@Named
 @Path("/")
 public class EndpointVersion {
   
-
+	@Path("/version")
     @GET
     public String getVersion() {
+        return "v1";
+    }
+
+    @GET
+    public String getVersionRoot() {
         return "v1";
     }
 
