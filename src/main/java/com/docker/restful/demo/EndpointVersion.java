934e1d262aba86b673541f525f0aee99c50946c5
lian <duanliandba@gmail.com>
2021-10-31 06:46:38 -0400

    Add V4 to support version

--- src/main/java/com/docker/restful/demo/EndpointVersion.java
+++ src/main/java/com/docker/restful/demo/EndpointVersion.java
@@ -6,18 +6,29 @@ package com.docker.restful.demo;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.docker.restful.demo.services.UserService;
+@RestController
 @Path("/")
 public class EndpointVersion {
-  
+	Logger logger = LoggerFactory.getLogger(EndpointVersion.class);
+	@Value("${app.version}")
+	private String appVersion;
 	@Path("/version")
     @GET
     public String getVersion() {
-        return "v2";
+		logger.info("appVersion is "+appVersion);
+        return appVersion;
     }
 
     @GET
     public String getVersionRoot() {
-        return "v2";
+    	logger.info("appVersion is "+appVersion);
+        return appVersion;
     }
 
   
