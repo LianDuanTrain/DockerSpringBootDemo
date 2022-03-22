0360eb1506c31b631ca73732d2777b80cba2c6d1
lian <duanliandba@gmail.com>
2022-03-22 06:51:20 -0400

    jetty version worked

--- src/main/java/com/docker/restful/demo/AppConfig.java
+++ src/main/java/com/docker/restful/demo/AppConfig.java
@@ -4,6 +4,7 @@ package com.docker.restful.demo;
 import javax.inject.Named;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +20,17 @@ public class AppConfig {
     @Named
     static class JerseyConfig extends ResourceConfig {
         public JerseyConfig() {
+        	try {
         	log.info("AppConfig version is V2");
             this.packages("com.docker.restful.demo.*");
             register(UserResource.class);
             register(SysInfoResource.class);
             register( EndpointVersion.class);
+            property(ServletProperties.FILTER_FORWARD_ON_404, true);
+
+            } catch (Exception e) {
+            	log.error("Exception: ", e);                   
+            }
         }
     }
 
